a39da5817963dd4a1642f187a7872b1641da249d
hvutuyen <hoang.vutuyen@live.de>
2020-05-10 15:02:57 +0200

    rm space

--- geoprojekt_syrien/settings.py
+++ geoprojekt_syrien/settings.py
@@ -156,5 +156,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
-
 django_heroku.settings(locals())
